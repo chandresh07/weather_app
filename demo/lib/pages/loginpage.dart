dbb1126b2528e0045f7dfaabec452ce10cd40077
Chandresh Rathod <chandreshrathod9465@gmail.com>
2024-07-11 08:56:46 +0530

    'authentication updated'

--- demo/lib/pages/loginpage.dart
+++ demo/lib/pages/loginpage.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'dart:convert';
 import 'package:demo/pages/homepage.dart';
 import 'package:demo/pages/signuppage.dart';
@@ -13,57 +10,55 @@ class loginpage extends StatefulWidget {
 }
 
 class _loginpageState extends State<loginpage> {
-
-  final _formKey = GlobalKey<FormState>();
-
-      String? validateEmail(String? email){
-        RegExp emailRegex = RegExp(r'^[\w\.-]+@[\w-]+\.\w{2,3}(\.\w{2,3})?$');
-        final isEmailValid = emailRegex.hasMatch(email ?? '');
-        if(!isEmailValid){
-          return "please Enter valid email";
-        }
-        return null;
-      }
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passController = TextEditingController();
 
-  Future<bool> login(String email , password) async {
+  Future<void> login() async {
+    String url =
+        'https://reqres.in/api/login'; // Replace with your API endpoint
+    var body = {
+      'email': _emailController.text,
+      'password': _passController.text,
+    };
     try {
-      final response = await http.post(
-          Uri.parse('https://reqres.in/api/login'),
-          body: {
-            'email': email,
-            'password': password
-          }
-      );
+      final response = await http.post(Uri.parse(url), body: body);
+      /*     final response = await http.post(Uri.parse('https://reqres.in/api/login'),
+          body: {'email': _emailController, 'password': _passController}); */
 
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body.toString());
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => homepage()));
         print(data['token']);
         print('Login successfully');
         return data['success'] ?? false;
       } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("wrong email or password")));
         print('failed');
         throw Exception('failed to authenticate');
       }
     } catch (e) {
       print(e.toString());
-      return false;
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('error:$e')));
+      // return false;
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: Text("Login"),
         centerTitle: true,
         backgroundColor: Colors.lightBlueAccent,
       ),
-      body:SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(20.0),
           child: Form(
-            key: _formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -73,32 +68,32 @@ class _loginpageState extends State<loginpage> {
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
                     prefixIcon: Icon(Icons.email_outlined),
-                      hintText: "email",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ) ,
-                  ),
-                  validator: validateEmail,
-                ),
-                SizedBox(height: 10,),
-                TextFormField(
-                  //obscureText: true,
-                  controller: _passController,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock),
-                      hintText: "password",
+                    hintText: "email",
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                 ),
-                SizedBox(height: 25,),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  //obscureText: true,
+                  controller: _passController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    hintText: "password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
                 GestureDetector(
-                  onTap: (){
-                    if(_formKey.currentState!.validate()){
-                      login(_emailController.text.toString(), _passController.text.toString());
-                      Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) => homepage()));
-                    }
+                  onTap: () {
+                    login();
                   },
                   child: Container(
                     height: 50,
@@ -112,23 +107,29 @@ class _loginpageState extends State<loginpage> {
                     ),
                   ),
                 ),
-                SizedBox(height: 10,),
+                SizedBox(
+                  height: 10,
+                ),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text("Don't have an account?"),
-                    TextButton(onPressed: (){
-                      Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (context) => signup()));
-                    }, child: Text("Click Here"))
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => signup()));
+                        },
+                        child: Text("Click Here"))
                   ],
                 )
               ],
             ),
           ),
         ),
-      ) ,
+      ),
     );
   }
 }
--- demo/lib/pages/signuppage.dart
+++ demo/lib/pages/signuppage.dart
@@ -1,6 +1,5 @@
-
 import 'dart:convert';
-import 'package:http/http.dart' ;
+import 'package:http/http.dart';
 import 'package:demo/pages/loginpage.dart';
 import 'package:flutter/material.dart';
 
@@ -10,104 +9,96 @@ class signup extends StatefulWidget {
 }
 
 class _signupState extends State<signup> {
-
-  final _formKey = GlobalKey<FormState>();
-
-  String? validateEmail(String? email){
-    RegExp emailRegex = RegExp(r'^[\w\.-]+@[\w-]+\.\w{2,3}(\.\w{2,3})?$');
-    final isEmailValid = emailRegex.hasMatch(email ?? '');
-    if(!isEmailValid){
-      return "please Enter valid email";
-    }
-    return null;
-  }
   TextEditingController emailcontroller = TextEditingController();
   TextEditingController passwordcontroller = TextEditingController();
 
-  void  login(String email , password) async {
+  Future<void> login() async {
+    String url = 'https://reqres.in/api/register';
+    var body = {
+      'email': emailcontroller.text,
+      'password': passwordcontroller.text
+    };
     try {
-      Response response = await post(
-          Uri.parse('https://reqres.in/api/register'),
-          body: {
-            'email': email,
-            'password': password
-          }
-      );
+      Response response = await post(Uri.parse(url), body: body);
 
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body.toString());
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => loginpage()));
         print(data['token']);
         print('Register successfully');
       } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("wrong email or password")));
         print('failed');
       }
     } catch (e) {
       print(e.toString());
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('error$e')));
     }
   }
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Sign Up"),
-          centerTitle: true,
-          backgroundColor: Colors.lightBlueAccent,
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding:EdgeInsets.all(20.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    controller: emailcontroller,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.email_outlined),
-                      hintText: "Enter Email",
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: validateEmail,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign Up"),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: emailcontroller,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email_outlined),
+                    hintText: "Enter Email",
+                    border: OutlineInputBorder(),
                   ),
-                  SizedBox(height: 10,),
-                  TextFormField(
-                    controller: passwordcontroller,
-                    obscureText: true,
-                    decoration: InputDecoration(
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: passwordcontroller,
+                  obscureText: true,
+                  decoration: InputDecoration(
                       prefixIcon: Icon(Icons.lock),
-                        hintText: "Enter Password",
-                      border: OutlineInputBorder()
+                      hintText: "Enter Password",
+                      border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    login();
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 120,
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlueAccent,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-
+                    child: Center(
+                      child: Text("Sign in"),
+                    ),
                   ),
-                  SizedBox(height: 20,),
-                  GestureDetector(
-                    onTap: (){
-                      if(_formKey.currentState!.validate()){
-                        login(emailcontroller.text.toString(), passwordcontroller.text.toString());
-                        Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) => loginpage()));
-                      }
-                    },
-                    child: Container(
-                      height: 50,
-                      width: 120,
-                      decoration: BoxDecoration(
-                        color: Colors.lightBlueAccent,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Text("Sign in"),
-                      ),
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             ),
           ),
         ),
-      );
-  }}
+      ),
+    );
+  }
+}
